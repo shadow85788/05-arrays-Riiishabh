408870b15a2062802de7ca507dd9cc3a96e5e03b
Riiishabh <84868932+Riiishabh@users.noreply.github.com>
2021-06-19 12:09:00 +0530

    Solution1

--- solns.c
+++ solns.c
@@ -1,2 +1,88 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int min(int arr[], int n)
+{
+  int min =arr[0];
+  for (int i=1; i<n;i++)
+  {
+    if (arr[i]<min)
+      min=arr[i];   
+  }
+  return min;
+}
+
+int max(int arr[], int n)
+{
+  int max =arr[0];
+  for (int i=1; i<n;i++)
+  {
+    if (arr[i]>max)
+      max=arr[i];   
+  }
+  return max;
+}
+
+float average(int arr[], int n)
+{
+  float avg=0;
+  for (int i=0; i<n;i++)
+    {avg+=arr[i];}
+  avg=(float)avg/(float)n;
+  return avg;
+}
+
+
+int mode (int arr[], int n)
+{
+  int max1=max(arr,n);
+  int ar[max1]={0};
+  for (int i=0; i<n; i++)
+  {
+    ar[arr[i]]++;
+  }
+  int maxxx=ar[0];
+  int j=0;
+  for (int k=1; k<max1;k++)
+  { 
+    if(ar[k]>maxxx)
+    {
+      maxxx=ar[k];
+      j=k;
+    }
+  }
+  return j;
+}
+
+
+int factors(int n, int arr[])
+{
+  int a=n;
+  int i=0;
+  while (a%2==0)
+  {
+    arr[i]=2;
+    ++i;
+    a/=2;
+  }
+  int f=sqrt(a);
+  for (int j=3;j<f;j+=2)
+  {
+    while (a%j==0)
+    {
+        arr[i]=j;
+        ++i;
+        a/=j;
+    }
+    if (a==1)
+      break;      
+  }
+  return i;
+}
+  
+  
+  
+  
